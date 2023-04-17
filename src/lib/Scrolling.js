efe6f280c81626e3263e08fdec21a2990e01d79d
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2023-04-18 00:05:34 +0900

    feat: scrolling 쉽지않군

--- src/lib/Scrolling.js
+++ src/lib/Scrolling.js
@@ -1,112 +1,107 @@
-import { useState, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 
 const Scrolling = () => {
-  const [itemNumber, setItemNumber] = useState(2);
-  const [itemWidth, setItemWidth] = useState(0);
+    const [itemWidth, setItemWidth] = useState(0);
+    const [itemNum, setItemNum] = useState(0);
+    const [count, setCount] = useState(0);
 
-  //   useEffect(() => {
-  //     setItemWidth(document.querySelector(".item2").clientWidth);
-  //     const _containerWidth =
-  //       document.querySelector(".scrollContainer").clientWidth;
-  //     if (_containerWidth % itemWidth === 0) {
-  //       setItemNumber(Math.floor(_containerWidth / itemWidth));
-  //     } else {
-  //       setItemNumber(Math.floor(_containerWidth / itemWidth) + 1);
-  //     }
-  //   }, [itemWidth]);
+    const containerRef = useRef(null);
+    const itemRef = useRef(null);
 
-  useEffect(() => {
-    if (!document.querySelector(".item2")?.clientWidth) return;
-    const _containerWidth =
-      document.querySelector(".scrollContainer").clientWidth;
-    const _itemWidth = document.querySelector(".item2")?.clientWidth;
+    useEffect(() => {
+        setItemWidth(itemRef.current.offsetWidth);
+        const _itemWidth = itemRef.current.offsetWidth;
+        const _containerWidth = containerRef.current.offsetWidth;
 
-    if (_containerWidth % itemWidth === 0) {
-      setItemNumber(Math.floor(_containerWidth / _itemWidth));
-    } else {
-      console.log(_containerWidth / _itemWidth);
-      setItemNumber(Math.floor(_containerWidth / _itemWidth));
-    }
+        setItemNum(Math.floor(_containerWidth / _itemWidth));
+        setCount(_containerWidth % _itemWidth);
 
-    setItemWidth(_itemWidth);
-  }, [itemWidth]);
+        console.log(_containerWidth % _itemWidth);
+    }, [itemWidth]);
 
-  return (
-    <Container>
-      <div className="scrollContainer primary">
-        <div className="item2">Scrolling!!!</div>
-        {Array.from({ length: itemNumber }).map((_, idx) => (
-          <div key={idx} className="item2">
-            Scrolling!!!
-          </div>
-        ))}
-      </div>
-      <div className="scrollContainer secondary">
-        {Array.from({ length: itemNumber }).map((_, idx) => (
-          <div key={`${idx}_sec`} className="item2">
-            Scrolling!!!
-          </div>
-        ))}
-      </div>
-    </Container>
-  );
+    return (
+        <Container ref={containerRef}>
+            <ScrollTrack>
+                <span className="div primary">
+                    <span className="scrollingContainer" ref={itemRef}>
+                        <span>안녕하세요오오오오</span>
+                    </span>
+                    {Array.from({ length: itemNum-1 }).map((_, idx) => (
+                        <span className="scrollingContainer" key={idx}>
+                            <span>안녕하세요오오오오</span>
+                        </span>
+                    ))}
+                </span>
+                <span className="div secondary">
+                    {Array.from({ length: itemNum }).map((_, idx) => (
+                        <span className="scrollingContainer" key={idx}>
+                            <span>안녕하세요오오오오</span>
+                        </span>
+                    ))}
+                </span>
+            </ScrollTrack>
+        </Container>
+    );
 };
 
-const primary = keyframes`
-	from {
-    left: 0%;
+const scrolling = keyframes`
+0% {
+    transform: translateX(0);
   }
-  to {
-    left: -100%;
+  100% {
+    transform: translateX(calc(-100% + 98px)); 
   }
 `;
 
-const secondary = keyframes`
-  from {
-    left: 100%;
+const scrolling2 = keyframes`
+0% {
+    transform: translateX(100%);
   }
-  to {
-    left: 0%;
+  100% {
+    transform: translateX(98px); 
   }
 `;
 
 const Container = styled.div`
-  position: relative;
-  width: 100%;
-  height: 80px;
-  display: flex;
-  flex-wrap: wrap;
-  overflow: hidden;
-  background-color: blue;
-  &:hover div {
-    animation-play-state: paused;
-  }
-
-  .item2 {
-    font-size: 30px;
-    font-weight: 700;
-    padding-right: 30px;
-  }
-
-  .scrollContainer {
     width: 100%;
     height: 80px;
+    background-color: blue;
+    display: flex;
+    flex-wrap: wrap;
+    overflow: hidden;
+`;
 
-    position: absolute;
-    left: 0%;
-    top: 0%;
+const ScrollTrack = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
     display: flex;
     align-items: center;
 
-    &.primary {
-      animation: ${primary} 5s linear infinite;
-    }
+    margin: 0;
+    padding: 0;
 
-    &.secondary {
-      animation: ${secondary} 5s linear infinite;
+    .div {
+        width: 100%;
+        height: 80px;
+
+        position: absolute;
+        left: 0%;
+        top: 0%;
+        display: flex;
+        align-items: center;
+        flex-wrap: wrap;
+
+        &.primary {
+            animation: ${scrolling} 5s linear infinite;
+        }
+
+        &.secondary {
+            left: -98px;
+            animation: ${scrolling2} 5s linear infinite;
+        }
     }
-  }
 `;
 
 export default Scrolling;
